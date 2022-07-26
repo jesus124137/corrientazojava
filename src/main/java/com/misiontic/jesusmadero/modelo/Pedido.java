c7b0c065dd901f6581d48ba41a3ec878745a41ae
Jesus Madero <jairoj.maderom@ecci.edu.co>
2022-07-26 18:44:53 -0500

    Agregada logica clase pedido

--- src/main/java/com/misiontic/jesusmadero/modelo/Pedido.java
+++ src/main/java/com/misiontic/jesusmadero/modelo/Pedido.java
@@ -1,20 +1,47 @@
 package com.misiontic.jesusmadero.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pedido {
 
-    public Integer calcularValor(){
-        //TODO: Calcular valor del pedido
-        return 0;
+    private String cliente;
+    private EstadoPedido estado;
+    private OpcionPedido opcion;
+    private List<Adicional> adicionales;
+
+    public Pedido(String cliente, OpcionPedido opcion) {
+        this.cliente = cliente;
+        this.opcion = opcion;
+
+        this.estado = EstadoPedido.PENDIENTE_ENTREGA;
+        adicionales = new ArrayList<>();
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public EstadoPedido getEstado() {
+        return estado;
+    }
+
+    public void agregarAdicional(Adicional adicional){
+        this.adicionales.add(adicional);
+    }
+
+    public Integer calcularValor() {
+        var total = opcion.getPrecio();
+        total += adicionales.stream()   
+                .map(item -> item.getPrecio())
+                .reduce((a, b) -> a + b)
+                .orElse(0);
+
+        return total;
     }
 
     public void entregar() {
+        estado = EstadoPedido.PENDIENTE_COBRAR;
     }
-
-
-
-
-
-
-
 
 }
